bf58a22375fdec3459d06bc8a630c7e5334891cb
raunak123455 <91775838+raunak123455@users.noreply.github.com>
2025-02-04 20:52:09 +0530

    Update ProductForm.tsx

--- components/ProductForm.tsx
+++ components/ProductForm.tsx
@@ -36,7 +36,7 @@ export default function ProductForm() {
   const fetchData = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch("http://localhost:5000/api/products/latest");
+      const response = await fetch("https://backend2-cbtt.onrender.com/api/products/latest");
       const apiData = await response.json();
       setData(apiData);
       setIsApiData(true);
